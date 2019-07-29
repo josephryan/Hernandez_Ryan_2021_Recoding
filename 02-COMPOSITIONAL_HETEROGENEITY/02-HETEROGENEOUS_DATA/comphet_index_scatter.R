4b26844652981ee9c636fc48e991d37817edc90b
Alexandra Hernandez <ahernandez6@whitney.ufl.edu>
2019-07-29 12:37:36 -0400

    added

---
+++ 02-COMPOSITIONAL_HETEROGENEITY/02-HETEROGENEOUS_DATA/comphet_index_scatter.R
@@ -0,0 +1,7 @@
+setwd("~/Dropbox/Dissertation/Ctenophore-Sponge_Debate/fig_comphet")
+data <- read.csv('comphet_index.csv')
+library(ggplot2)
+p <-ggplot(data, aes(x=Tree, y=Comp.het.index)) + geom_point() + facet_wrap(vars(Inflation.parameter), scales = "free", dir="v")
+p + scale_x_reverse(name="Branch length", breaks=c(0.008, 0.004, 0.002, 0.001)) + scale_y_continuous(name="Comp-het index") + labs (color="Inflation parameter") + geom_smooth(method=lm, se=FALSE)
+
+                                                                                                                                         
