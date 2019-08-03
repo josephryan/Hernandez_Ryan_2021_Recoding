814471fe7cf163f3cba713619668a9c3b4a57027
Alexandra Hernandez <alex@ssrb-vpn2-11-189.vpn.ufl.edu>
2019-08-03 13:42:09 -0400

    added shell script and updated README.md

--- 03-SATURATION/04-ESTIMATED_MODEL/README.md
+++ 03-SATURATION/04-ESTIMATED_MODEL/README.md
@@ -2,3 +2,6 @@
 
 ### run_seqgen_estimated_model.sh
 shell script to reproduce simulations performed in Seq-Gen
+
+### divide_estimated_model.sh
+shell script to create new directories for simulated datasets and parse into individual PHYLIP files
---
+++ 03-SATURATION/04-ESTIMATED_MODEL/divide_estimated_model.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.1.phy Chang.mismatch.1
+perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.5.phy Chang.mismatch.5
+perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.10.phy Chang.mismatch.10
+perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.15.phy Chang.mismatch.15
+perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.20.phy Chang.mismatch.20
