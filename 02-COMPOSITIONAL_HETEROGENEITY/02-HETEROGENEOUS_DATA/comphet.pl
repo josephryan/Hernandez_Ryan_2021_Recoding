9484b29ba1a247dd7008497e6cb1d0c311b71795
ahernandez6 <ahernandez6@ufl.edu>
2020-12-28 15:38:54 -0500

    Updated version of comphet.pl

--- 02-COMPOSITIONAL_HETEROGENEITY/02-HETEROGENEOUS_DATA/comphet.pl
+++ 02-COMPOSITIONAL_HETEROGENEITY/02-HETEROGENEOUS_DATA/comphet.pl
@@ -3,25 +3,26 @@ use strict;
 use warnings;
 use Data::Dumper;
 use List::Util qw(shuffle);
-use lib qw(/Hernandez_Ryan_2019_RecodingSim/01-MODULES/);
+use lib qw(/bwdata1/ahernandez6/07-RECODING_ANALYSIS/06-PUB_SCRIPTS/JFR/);
 use CompHet;
 use Cwd;
 
-our $VERSION = 0.07;
+our $VERSION = 0.10;
 
 our $DIR = Cwd::getcwd();
 our $REPS = 1000; # number of replicates for each dataset 
 our @POSSIBLES = (1..1000000000);
-our $P4_TEST= 'test.py';
 our $NUM_INFL = 1; # number of datasets for each replicate 
                    # each set increments inflation parameter by $INFL
 MAIN: {
-    my $tree = $ARGV[0] or die "usage $0 TREE000X INFL NULL\n";
-    my $infl = $ARGV[1] or die "usage $0 TREE000X INFL NULL\n";
-    my $null = $ARGV[2] or die "usage $0 TREE000x INFL NULL\n";
+    my $tree = $ARGV[0] or die "usage $0 TREE000X INFL LENGTH PAIRINGS NULL\n";
+    my $infl = $ARGV[1] or die "usage $0 TREE000X INFL LENGTH PAIRINGS NULL\n";
+    my $length = $ARGV[2] or die "usage $0 TREE000x INFL LENGTH PAIRINGS NULL\n";
+    my $pair = $ARGV[3] or die "usage $0 TREE000x INFL LENGTH PAIRINGS NULL\n"; 
+    my $null = $ARGV[4] or die "usage $0 TREE000x INFL LENGTH PAIRINGS NULL\n";
     my $tree_var = JFR::CompHet::get_tree_var($tree);
     my @shuf = shuffle(@POSSIBLES);
-    run_simulations($tree_var,$infl, \@shuf);
+    run_simulations($tree_var, $infl, $length, $pair, \@shuf);
     my $rh_seqs = get_seqs($DIR, $infl); 
     my $rh_freqs = get_freqs($rh_seqs); 
     my $rh_test_comphets = get_comphets($rh_freqs); 
@@ -83,12 +84,15 @@ sub get_freqs {
 sub run_simulations{
     my $tree = shift;
     my $infl = shift;
+    my $length = shift;
+    my $pair = shift;
     my $ra_shuf = shift;
     foreach (my $f=$infl;  $f <= ($infl * $NUM_INFL); $f += $infl){
-        my $rh_f = get_start_freqs($f);
         for (my $i=0; $i < $REPS; $i++){
+            my ($ra_first10, $ra_last10) = get_chang_freqs($pair);
+            my $rh_f = get_start_freqs($f, $ra_first10, $ra_last10);
             my $seed = shift @{$ra_shuf};
-            write_and_run_pycode ($tree, $seed, $f, $rh_f->{$f});
+            write_and_run_pycode ($tree, $length, $seed, $f, $rh_f->{$f});
             unlink("$seed.$f.nex");
         }
     }
@@ -96,23 +100,37 @@ sub run_simulations{
 
 sub write_and_run_pycode {
     my $tree = shift;
+    my $length = shift;
     my $seed = shift;
     my $f = shift;
     my $rh_f = shift;
+    my $file = "seed$seed.$f.py";
+    my $taxa = $tree;
+    $taxa =~ s/\s//g;
+    $taxa =~ s/^\(+//g;
+    $taxa =~ s/:[0-9\.]+//g;
+    my @taxa = split/[,)(;]/, $taxa;
+    foreach my $t (@taxa){
+        $t = "'$t'" if ($t);
+    }
+    my $str = join ",", @taxa;
+    $str =~ s/,+/,/g;
+    my ($root, $node1, $node2) = JFR::CompHet::get_nodes($tree);
     my $xstr = join ',', @JFR::CompHet::CHANG_FREQ;
     my $ystr = join ',', @{$rh_f};
     my $chang_r = join ',', @JFR::CompHet::CHANG_RATE;
-    open OUT, ">$P4_TEST" or die "cannot open $P4_TEST:$!";
+    mkdir 'p4_scripts' unless -d 'scripts';
+    open OUT, ">p4_scripts/$file" or die "cannot open $file:$!";
     print OUT  qq~func.reseedCRandomizer($seed)
 read("$tree")
 t = var.trees[0]
-a = func.newEmptyAlignment(dataType='protein', taxNames=['A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'B4', 'B5', 'C1', 'C2', 'C3', 'C4', 'C5', 'D1', 'D2', 'D3', 'D4', 'D5'], length=1000)
+a = func.newEmptyAlignment(dataType='protein', taxNames=[$str], length=$length)
 t.data = Data([a])
 x = t.newComp(free=1, spec='specified', symbol='A', val=[$xstr])
 y = t.newComp(free=1, spec='specified', symbol='B', val=[$ystr])
-t.setModelThing(x, node=t.root, clade=1)
-t.setModelThing(y, node=2, clade=1)
-t.setModelThing(y, node=21, clade=1)
+t.setModelThing(x, node=$root, clade=1)
+t.setModelThing(y, node=$node1, clade=1)
+t.setModelThing(y, node=$node2, clade=1)
 t.newRMatrix(free=1, spec='specified', val=[$chang_r])
 t.setNGammaCat(nGammaCat=4)
 t.newGdasrv(free=1, val=0.5)
@@ -124,25 +142,41 @@ read("$seed.$f.nex")
 a=var.alignments[0]
 a.writePhylip(fName="$seed.$f.phy", interleave=False,whitespaceSeparatesNames=True, flat=True)
 ~;
+
     close OUT;
-    system "p4 $P4_TEST > /dev/null 2> /dev/null";
+    system "p4 p4_scripts/$file > /dev/null 2> /dev/null";
+}
+
+sub get_chang_freqs{
+    my $pair = shift;
+    my @first10 = ();
+    my @last10 = ();
+    if ($pair =~ m/random/i){
+        my @freqs = shuffle(@JFR::CompHet::CHANG_FREQ); 
+        @first10 = @freqs[0..9];
+        @last10 = @freqs[10..19];
+    }elsif ($pair =~ m/alphabetical/i){
+        @first10 = @JFR::CompHet::CHANG_FREQ[0..9];
+        @last10 = @JFR::CompHet::CHANG_FREQ[10..19];
+    }  
+    return (\@first10, \@last10);
 }
 
 sub get_start_freqs{
     my $f = shift;
+    my $first10 = shift;
+    my $last10 = shift;
     my %rh_f = ();
-    my @first10 = @JFR::CompHet::CHANG_FREQ[0..9];
-    my @last10 = @JFR::CompHet::CHANG_FREQ[10..19];
-        for (my $i = 0; $i < @first10; $i++){
-            if ($first10[$i] >= $last10[$i]){
+        for (my $i = 0; $i < @{$first10}; $i++){
+          if (@{$first10}[$i] >= @{$last10}[$i]){
                 # round so p4 doesn't croak w >3 digits after decimal point
-                my $val = sprintf("%.3f",($last10[$i]*$f));
-                $rh_f{$f}->[$i]    = $first10[$i] - $val;
-                $rh_f{$f}->[$i+10] = $last10[$i] + $val;
+                my $val = sprintf("%.3f",(@{$last10}[$i]*$f));
+                $rh_f{$f}->[$i]    = @{$first10}[$i] - $val;
+                $rh_f{$f}->[$i+10] = @{$last10}[$i] + $val;
             } else {
-                my $val = sprintf("%.3f",($first10[$i]*$f));
-                $rh_f{$f}->[$i]    = $first10[$i] + $val;
-                $rh_f{$f}->[$i+10] = $last10[$i] - $val;
+                my $val = sprintf("%.3f",(@{$first10}[$i]*$f));
+                $rh_f{$f}->[$i]    = @{$first10}[$i] + $val;
+                $rh_f{$f}->[$i+10] = @{$last10}[$i] - $val;
             }
         }    
     return \%rh_f;
