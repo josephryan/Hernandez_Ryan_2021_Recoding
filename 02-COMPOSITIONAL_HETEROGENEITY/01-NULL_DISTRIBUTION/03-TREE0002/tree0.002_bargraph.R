efa5477ce78b743809f72706862d05bb9ddb0a72
ahernandez6 <ahernandez6@wh-maxine.whitney.ufl.edu>
2021-02-02 16:21:01 -0500

    altered script to ajust for correct legend in figure produced

--- 02-COMPOSITIONAL_HETEROGENEITY/01-NULL_DISTRIBUTION/03-TREE0002/tree0.002_bargraph.R
+++ 02-COMPOSITIONAL_HETEROGENEITY/01-NULL_DISTRIBUTION/03-TREE0002/tree0.002_bargraph.R
@@ -8,5 +8,5 @@ data$inflation.parameter[data$inflation.parameter == "0"] <- "0.25"
 data$inflation.parameter[data$inflation.parameter == "0.1"] <- "0.50"
 data$inflation.parameter[data$inflation.parameter == "0.5"] <- "0.75"
 data$inflation.parameter[data$inflation.parameter == "0.9"] <- "1.00"
-b <- ggplot(data, aes(x=inflation.parameter, y=percent)) + geom_bar(aes(fill=type), stat = "identity", position=position_dodge2(preserve = c("total"), padding = 0.2)) 
+b <- ggplot(data, aes(x=inflation.parameter, y=percent)) + geom_bar(aes(fill=reorder(type, percent)), stat = "identity", position=position_dodge2(preserve = c("total"), padding = 0.2)) 
 b + scale_x_discrete(name="Inflation parameter (level of compositional heterogeneity)", breaks =c("0.25", "0.50", "0.75", "1.00"), label=c("0", "0.1","0.5","0.9")) + scale_y_continuous(name="Percentage of incorrect trees (%)", expand = c(0, 0), limits=c(0,70)) + labs(fill="Matrix") + scale_fill_manual(values=c("#bcbddc", "#756bb1","#fdae6b", "#e6550d"))
