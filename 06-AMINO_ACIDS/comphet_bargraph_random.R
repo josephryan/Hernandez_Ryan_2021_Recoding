f74242b1a3dd8e3b9dfa4e3e5d5515f3496bf252
ahernandez6 <ahernandez6@wh-maxine.whitney.ufl.edu>
2021-04-02 13:54:43 -0400

    updated labels

--- 06-AMINO_ACIDS/comphet_bargraph_random.R
+++ 06-AMINO_ACIDS/comphet_bargraph_random.R
@@ -4,3 +4,4 @@ names(data)[4]<-"percent"
 library(ggplot2)
 b <- ggplot(data, aes(x=inflation.parameter, y=percent)) + geom_bar(aes(fill=reorder(type, percent)), stat = "identity", position=position_dodge2(preserve = c("total"), padding = 0.2))  + facet_wrap(~Pairing, nrow=1)
 b + scale_x_continuous(name="Inflation parameter (level of compositional heterogeneity)", breaks =c(0.1, 0.5, 0.9), label=c("0.1","0.5","0.9"), expand = c(0, 0)) + scale_y_continuous(name="Percentage of incorrect trees (%)", expand = c(0, 0), limits=c(0,70)) + labs(fill="Matrix") + scale_fill_manual(values=c("#756bb1","#e6550d"))
+#Figure was modified in illustrator for supplemental figure
