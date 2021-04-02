8481e781f738b1794dfe48e7ce87a8fee093f19c
ahernandez6 <ahernandez6@wh-maxine.whitney.ufl.edu>
2021-04-01 23:55:01 -0400

    updated file paths

--- 06-AMINO_ACIDS/comphet_bargraph_random.R
+++ 06-AMINO_ACIDS/comphet_bargraph_random.R
@@ -1,7 +1,6 @@
-setwd("/Hernandez_Ryan_2019_RecodingSim/07-RCFV/")
-data <- read.csv('comp_het_random_proportion.csv')
+data <- read.csv('comp_het_random_proportion_v2.csv')
 data$proportion <- data$proportion*100
-names(data)[3]<-"percent"
+names(data)[4]<-"percent"
 library(ggplot2)
-b <- ggplot(data, aes(x=inflation.parameter, y=percent)) + geom_bar(aes(fill=type), stat = "identity", position=position_dodge2(preserve = c("total"), padding = 0.2)) 
+b <- ggplot(data, aes(x=inflation.parameter, y=percent)) + geom_bar(aes(fill=reorder(type, percent)), stat = "identity", position=position_dodge2(preserve = c("total"), padding = 0.2))  + facet_wrap(~Pairing, nrow=1)
 b + scale_x_continuous(name="Inflation parameter (level of compositional heterogeneity)", breaks =c(0.1, 0.5, 0.9), label=c("0.1","0.5","0.9"), expand = c(0, 0)) + scale_y_continuous(name="Percentage of incorrect trees (%)", expand = c(0, 0), limits=c(0,70)) + labs(fill="Matrix") + scale_fill_manual(values=c("#756bb1","#e6550d"))
