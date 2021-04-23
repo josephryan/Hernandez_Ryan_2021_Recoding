e52a854db1fa86cb37b817f08f664cdc7f4b60a5
ahernandez6 <ahernandez6@ufl.edu>
2021-04-23 13:52:07 -0400

    Updated correct file path

--- 03-SATURATION/04-ESTIMATED_MODEL/divide_estimated_model.sh
+++ 03-SATURATION/04-ESTIMATED_MODEL/divide_estimated_model.sh
@@ -1,6 +1,7 @@
+[ahernandez6@wh-maxine 02-JTT]$ vi README.md
 #!/bin/sh
-perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.1.phy Chang.mismatch.1
-perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.5.phy Chang.mismatch.5
-perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.10.phy Chang.mismatch.10
-perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.15.phy Chang.mismatch.15
-perl /Hernandez_Ryan_2019_RecodingSim/03-SATURATION/divide.pl Chang.mismatch.20.phy Chang.mismatch.20
+perl ../divide.pl Chang.mismatch.1.phy Chang.mismatch.1
+perl ../divide.pl Chang.mismatch.5.phy Chang.mismatch.5
+perl ../divide.pl Chang.mismatch.10.phy Chang.mismatch.10
+perl ../divide.pl Chang.mismatch.15.phy Chang.mismatch.15
+perl ../divide.pl Chang.mismatch.20.phy Chang.mismatch.20
