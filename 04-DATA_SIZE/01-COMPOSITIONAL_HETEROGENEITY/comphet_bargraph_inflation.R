f6ce874b0f25e6dd7b00d69be683ff2190afdaec
ahernandez6 <ahernandez6@ufl.edu>
2021-04-24 14:54:23 -0400

    Add files via upload

---
+++ 04-DATA_SIZE/01-COMPOSITIONAL_HETEROGENEITY/comphet_bargraph_inflation.R
@@ -0,0 +1,6 @@
+data <- read.csv('comp_het_2000.5.9_proportion.csv')
+data$proportion <- data$proportion*100
+names(data)[3]<-"percent"
+library(ggplot2)
+b <- ggplot(data, aes(x=inflation.parameter, y=percent)) + geom_bar(aes(fill=type), stat = "identity", position=position_dodge2(preserve = c("total"), padding = 0.2)) 
+b + scale_x_continuous(name="Inflation parameter (level of compositional heterogeneity)", breaks =c(0.5, 0.6, 0.7, 0.8, 0.9), label=c("0.5","0.6","0.7", "0.8", "0.9"), expand = c(0, 0)) + scale_y_continuous(name="Percentage of incorrect trees (%)", expand = c(0, 0), limits=c(0,70)) + labs(fill="Matrix") + scale_fill_manual(values=c("#756bb1","#e6550d"))
